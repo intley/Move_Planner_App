f08b0cfaa3a120fd5ebbfd239b0a8a5d61601bf8
vagrant <vagrant@vagrant.vm>
2017-11-08 16:16:52 +0000

    NY Times and Wikipedia Articles returned!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
 function loadData() {
 
+    /*
+    The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
+    */
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -11,15 +14,57 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
     // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    // obviously, replace all the "X"s with your own API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=02a5b573943348058b82c7fc37e74f83';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    var wikilink='http://en.wikipedia.org/w/api.php?action=opensearch&search='+cityStr+'&format=json';
+    $.ajax({
+      url: wikilink,
+      dataType: 'jsonp',
+      success: function(response) {
+        var wikiarticles = response[1];
+
+        for (var i=0; i < wikiarticles.length; i++) {
+          wikiart = wikiarticles[i];
+          var url = "http://en.wikipedia.org/wiki/" + wikiart;
+          $wikiElem.append('<li><a href="' +url+ '">' + wikiart + '</li>');
+        }
+      }
+
+    })
+
+    // load wikipedia data
 
     // YOUR CODE GOES HERE!
-    var street = $('#street').val();
-    var city = $('#city').val();
-    var address = street + ', ' + city;
-
-    var streetviewurl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class ="bgimg" src="' + streetviewurl + '">');
 
     return false;
 };
